91ba38e9f91c999cb5d6b7a190ea55de1bf80d13
Farhan Mannan <farhanmannan@mac.com>
2012-04-01 23:48:02 +0100

    added arrays

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function next() { 
     document.getElementById("exercise").textContent = "E major scale";
     document.getElementById("position").innerHTML = "5";
-}
+};
 
+var scales = [
+    "minor",
+    "major",
+    "minor pentatonic",
+    "major pentatonic"
+];
+
+var positions = [1, 2, 3, 4, 5];
+
+var letters = ["A", "B", "C", "D", "E", "F", "G"];
+
+var accidentals = ["", "♯", "♭"];
+
+for (i = 0; i <= 2; i += 1) {
+    document.writeln(accidentals[i]);
+}
