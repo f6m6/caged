fe55f4ebee7ca4cad2343b9f5ab988cc134afb22
Farhan Mannan <farhanmannan@mac.com>
2012-04-02 12:43:25 +0100

    all required functionality done

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
+function init() {
+    next();
+}
+
+window.onload = init;
+
 function next() { 
-    document.getElementById("exercise").textContent = "E major scale";
-    document.getElementById("position").innerHTML = "5";
-};
+    document.getElementById("exercise").textContent = randomExercise();
+    document.getElementById("position").innerHTML = randomPosition();
+    document.getElementById("next").focus();
+}
+
+function random(n) {
+    return Math.floor(Math.random()*(1+n));
+}
+
+function randomPosition() {
+    return 1 + random(4);
+}
+
+function randomScale() {
+    return scales[random(3)];
+}
+
+function randomLetter() {
+    return letters[random(6)];
+}
+
+function randomAccidental() {
+    return accidentals[random(2)];
+}
+
+function randomExercise() {
+    return randomLetter() +  randomAccidental() + " " + randomScale() + " scale";
+}
 
 var scales = [
     "minor",
     "major",
     "minor pentatonic",
     "major pentatonic"
-];
+]
 
-var positions = [1, 2, 3, 4, 5];
+var positions = [1, 2, 3, 4, 5]
 
-var letters = ["A", "B", "C", "D", "E", "F", "G"];
+var letters = ["A", "B", "C", "D", "E", "F", "G"]
 
-var accidentals = ["", "♯", "♭"];
-
-for (i = 0; i <= 2; i += 1) {
-    document.writeln(accidentals[i]);
-}
+var accidentals = ["", "\u266f", "\u266d"]
